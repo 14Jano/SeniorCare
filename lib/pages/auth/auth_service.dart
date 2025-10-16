ddb97095fc0ae0317d559dec5bb89e051055b79f
Jan Łabaj <jan.labaj@studenci.collegiumwitelona.pl>
2025-10-16 23:54:01 +0200

    dodanie opcji zalogowania sie przez konto google

--- lib/pages/auth/auth_service.dart
+++ lib/pages/auth/auth_service.dart
@@ -1,6 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
-  final _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return null;
+      
+      final googleAuth = await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      print(e.toString());
+    }
+    return null;
+  }
+
 
   Future<void> sendEmailVerification() async {
     try {
